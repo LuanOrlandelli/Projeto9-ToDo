269397fff6baf26baf36e6387a9916352cd3cecc
LuanOrlandelli <luanorlandelli0112@gmail.com>
2024-05-02 10:57:35 -0300

    Criando a função validar campo

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+//Declarandovariavéis
+let tarefas = [];
+
+//Função que valida o preenchimento do campo, caso esteja vazio
+function validaCampo(){
+    let valida = false;
+    if(document.getElementById("task").value == "")
+    valida = true;
+    return valida
+}
+
