2dc63a8c8cb35ddb25fcc3a7721f174cf5c4d74e
LuanOrlandelli <luanorlandelli0112@gmail.com>
2024-05-02 11:03:23 -0300

    Criando a função adicionar tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,20 @@ function validaCampo(){
     return valida
 }
 
+//Função que adiciona tarefa
+function btnAdicionar(){
+    //variavel que vai receber as tarefas
+    let linhas = document.getElementById("task");
+
+    if (validaCampo(0)) {
+        alert("Preencha o campo com a tarefa!")
+    }else{
+        //Adicionando as tarefas
+        tarefas.push(linhas.value);
+        //passando para a proxima linha vazia 
+        linhas.value="";
+        //chamando a função que vai mostrar todas as tarefas
+        listarTarefas();
+    }
+    document.getElementById("taks").focus();
+}
